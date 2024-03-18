c25e6d4a61b6315e9db0bb549447917833091f17
madCoder <anasswakac@gmail.com>
2024-03-18 00:52:17 +0000

    automating

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ try{
  server.get('/', (req, res) =>{
    //  res.render('index');
     res.send('......'+data);
-    consol.log("data "+data);
+   //  console.log("data "+data);
  });
 
  server.listen(port, ()=>{
